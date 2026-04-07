f22f0a393e7e1e225db7410c902d25a1137bfff6
Franco Rodríguez <jeannfrancorodriguez@gmail.com>
2026-04-07 05:15:24 -0400

    Update footer and header links and revise site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sistemas de Información Blog';
-export const SITE_DESCRIPTION = 'Un blog sobre Sistemas de Información, Tecnología y Programación.';
+export const SITE_TITLE = 'Blog de Sistemas de Información';
+export const SITE_DESCRIPTION =
+	'Blog colaborativo del proyecto de Sistemas de Información del grupo integrado por Kevin Zambrano, Jeann Rodríguez, Frankly Bautista y Leonardo Lobo.';
